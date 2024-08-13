d2d1dd48f53307263cf0d02d6fa3ecb88dfa5ca5
philip-persan <phillipeslash@gmail.com>
2024-08-13 14:37:28 -0300

    first commit & initial config

---
+++ users/models.py
@@ -0,0 +1,38 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    id = models.UUIDField(
+        verbose_name='id',
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+    email = models.EmailField(
+        verbose_name='email',
+        unique=True,
+        blank=False,
+        null=False
+    )
+    telefone = models.CharField(
+        max_length=20,
+        verbose_name='telefone',
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Data de Criação'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Data de Atualização'
+    )
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['created_at', ]
